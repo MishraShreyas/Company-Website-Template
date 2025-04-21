254b60d0728dd32cf014d7208745383df9921685
Shreyas Mishra <shreyasmishra03@gmail.com>
2025-04-22 03:30:45 +0530

    sidebar bug

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,16 +1,8 @@
-"use client";
+"use server";
 
-import { SidebarLayout as Sidebar } from "@/components/layout/Sidebar";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	// User is authenticated, render the main layout
-	return (
-		<div className="flex h-screen bg-background">
-			<Sidebar />
-			<div className="flex flex-1 flex-col overflow-hidden">
-				{children}
-			</div>
-		</div>
-	);
+	return <>{children}</>;
 }
